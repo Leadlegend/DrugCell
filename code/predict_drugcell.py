034bbf4eda49d97688e25303b607633907548a9f
kay <kaixinzhang@pku.edu.cn>
2022-05-05 03:32:11 +0800

    slightly beautify the original code

--- code/predict_drugcell.py
+++ code/predict_drugcell.py
@@ -8,55 +8,55 @@ import torch.nn as nn
 import torch.nn.functional as F
 import util
 from util import *
-from drugcell_NN import *
+from model.drugcell import *
 import argparse
 
 
 def predict_dcell(predict_data, gene_dim, drug_dim, model_file, hidden_folder, batch_size, result_file, cell_features, drug_features):
 
-	feature_dim = gene_dim + drug_dim
+    feature_dim = gene_dim + drug_dim
 
-	model = torch.load(model_file, map_location='cuda:%d' % CUDA_ID)
+    model = torch.load(model_file, map_location='cuda:%d' % CUDA_ID)
 
-	predict_feature, predict_label = predict_data
+    predict_feature, predict_label = predict_data
 
-	predict_label_gpu = predict_label.cuda(CUDA_ID)
+    predict_label_gpu = predict_label.cuda(CUDA_ID)
 
-	model.cuda(CUDA_ID)
-	model.eval()
+    model.cuda(CUDA_ID)
+    model.eval()
 
-	test_loader = du.DataLoader(du.TensorDataset(predict_feature,predict_label), batch_size=batch_size, shuffle=False)
+    test_loader = du.DataLoader(du.TensorDataset(predict_feature,predict_label), batch_size=batch_size, shuffle=False)
 
-	#Test
-	test_predict = torch.zeros(0,0).cuda(CUDA_ID)
-	term_hidden_map = {}	
+    #Test
+    test_predict = torch.zeros(0,0).cuda(CUDA_ID)
+    term_hidden_map = {}    
 
-	batch_num = 0
-	for i, (inputdata, labels) in enumerate(test_loader):
-		# Convert torch tensor to Variable
-		features = build_input_vector(inputdata, cell_features, drug_features)
+    batch_num = 0
+    for i, (inputdata, labels) in enumerate(test_loader):
+        # Convert torch tensor to Variable
+        features = build_input_vector(inputdata, cell_features, drug_features)
 
-		cuda_features = Variable(features.cuda(CUDA_ID), requires_grad=False)
+        cuda_features = Variable(features.cuda(CUDA_ID), requires_grad=False)
 
-		# make prediction for test data
-		aux_out_map, term_hidden_map = model(cuda_features)
+        # make prediction for test data
+        aux_out_map, term_hidden_map = model(cuda_features)
 
-		if test_predict.size()[0] == 0:
-			test_predict = aux_out_map['final'].data
-		else:
-			test_predict = torch.cat([test_predict, aux_out_map['final'].data], dim=0)
+        if test_predict.size()[0] == 0:
+            test_predict = aux_out_map['final'].data
+        else:
+            test_predict = torch.cat([test_predict, aux_out_map['final'].data], dim=0)
 
-		for term, hidden_map in term_hidden_map.items():
-			hidden_file = hidden_folder+'/'+term+'.hidden'
-			with open(hidden_file, 'ab') as f:
-				np.savetxt(f, hidden_map.data.cpu().numpy(), '%.4e')
+        for term, hidden_map in term_hidden_map.items():
+            hidden_file = hidden_folder+'/'+term+'.hidden'
+            with open(hidden_file, 'ab') as f:
+                np.savetxt(f, hidden_map.data.cpu().numpy(), '%.4e')
 
-		batch_num += 1
+        batch_num += 1
 
-	test_corr = pearson_corr(test_predict, predict_label_gpu)
-	print("Test pearson corr\t%s\t%.6f" % (model.root, test_corr))
+    test_corr = pearson_corr(test_predict, predict_label_gpu)
+    print("Test pearson corr\t%s\t%.6f" % (model.root, test_corr))
 
-	np.savetxt(result_file+'/drugcell.predict', test_predict.cpu().numpy(),'%.4e')
+    np.savetxt(result_file+'/drugcell.predict', test_predict.cpu().numpy(),'%.4e')
 
 
 parser = argparse.ArgumentParser(description='Train dcell')
@@ -91,4 +91,4 @@ CUDA_ID = opt.cuda
 
 print("Total number of genes = %d" % num_genes)
 
-predict_dcell(predict_data, num_genes, drug_dim, opt.load, opt.hidden, opt.batchsize, opt.result, cell_features, drug_features)	
+predict_dcell(predict_data, num_genes, drug_dim, opt.load, opt.hidden, opt.batchsize, opt.result, cell_features, drug_features)    
--- code/util.py
+++ code/util.py
@@ -1,156 +1,201 @@
 import sys
 import torch
-import networkx as nx
-import networkx.algorithms.components.connected as nxacc
-import networkx.algorithms.dag as nxadag
+import argparse
 import numpy as np
+import networkx as nx
+import networkx.algorithms.dag as nxadag
+import networkx.algorithms.components.connected as nxacc
+
+
+def args_util():
+    # load the params for training using argparse
+    parser = argparse.ArgumentParser(description='Train dcell')
+    parser.add_argument(
+        '-onto', help='Ontology file used to guide the neural network', type=str)
+    parser.add_argument('-train', help='Training dataset', type=str)
+    parser.add_argument('-val', help='Validation dataset', type=str)
+    parser.add_argument(
+        '-epoch', help='Training epochs for training', type=int, default=300)
+    parser.add_argument('-lr', help='Learning rate', type=float, default=0.001)
+    parser.add_argument('-optimizer', help="Optimizor",
+                        type=str, default="adam")
+    parser.add_argument(
+        '-sched', help="Learning rate scheduler", type=str, default="None")
+    parser.add_argument('-batch', help='Batchsize', type=int, default=5000)
+    parser.add_argument(
+        '-ckpt', help='Path of model checkpoint', type=str, default=None)
+    parser.add_argument(
+        '-save_dir', help='Folder for trained models', type=str, default='./model/')
+    parser.add_argument('-device', help='Specify GPU', type=int, default=0)
+    parser.add_argument('-gene2id', help='Gene to ID mapping file', type=str)
+    parser.add_argument('-drug2id', help='Drug to ID mapping file', type=str)
+    parser.add_argument('-cell2id', help='Cell to ID mapping file', type=str)
+
+    parser.add_argument(
+        '-geno_hid', help='Mapping for the number of neurons in each term in genotype parts', type=int, default=6)
+    parser.add_argument(
+        '-drug_hid', help='Mapping for the number of neurons in each layer', type=str, default='100,50,6')
+    parser.add_argument(
+        '-final_hid', help='The number of neurons in the top layer', type=int, default=6)
+
+    parser.add_argument(
+        '-cell_embed', help='Mutation information for cell lines', type=str)
+    parser.add_argument(
+        '-drug_embed', help='Morgan fingerprint representation for drugs', type=str)
+
+    return parser.parse_args()
+
 
 def pearson_corr(x, y):
-	xx = x - torch.mean(x)
-	yy = y - torch.mean(y)
+    xx = x - torch.mean(x)
+    yy = y - torch.mean(y)
 
-	return torch.sum(xx*yy) / (torch.norm(xx, 2)*torch.norm(yy,2))
+    return torch.sum(xx*yy) / (torch.norm(xx, 2)*torch.norm(yy, 2))
+
 
 def load_ontology(file_name, gene2id_mapping):
 
-	dG = nx.DiGraph()
-	term_direct_gene_map = {}
-	term_size_map = {}
+    dG = nx.DiGraph()
+    term_direct_gene_map = {}
+    term_size_map = {}
 
-	file_handle = open(file_name)
+    file_handle = open(file_name)
 
-	gene_set = set()
+    gene_set = set()
 
-	for line in file_handle:
+    for line in file_handle:
 
-		line = line.rstrip().split()
-		
-		if line[2] == 'default':
-			# add edge between subsystems
-			dG.add_edge(line[0], line[1])
-		else:
-			# add edges between subsystem and gene
-			# where line0 is subsys and line1 is gene
-			if line[1] not in gene2id_mapping:
-				continue
+        line = line.rstrip().split()
 
-			if line[0] not in term_direct_gene_map:
-				term_direct_gene_map[ line[0] ] = set()
+        if line[2] == 'default':
+            # add edge between subsystems
+            dG.add_edge(line[0], line[1])
+        else:
+            # add edges between subsystem and gene
+            # where line0 is subsys and line1 is gene
+            if line[1] not in gene2id_mapping:
+                continue
 
-			term_direct_gene_map[line[0]].add(gene2id_mapping[line[1]])
+            if line[0] not in term_direct_gene_map:
+                term_direct_gene_map[line[0]] = set()
 
-			gene_set.add(line[1])
+            term_direct_gene_map[line[0]].add(gene2id_mapping[line[1]])
 
-	file_handle.close()
+            gene_set.add(line[1])
 
-	print('There are', len(gene_set), 'genes')
+    file_handle.close()
 
-	for term in dG.nodes():
-		# calculate the number of gene relating with every term
-		term_gene_set = set()
+    print('There are', len(gene_set), 'genes')
 
-		if term in term_direct_gene_map:
-			term_gene_set = term_direct_gene_map[term]
+    for term in dG.nodes():
+        # calculate the number of gene relating with every term
+        term_gene_set = set()
 
-		deslist = nxadag.descendants(dG, term)
+        if term in term_direct_gene_map:
+            term_gene_set = term_direct_gene_map[term]
 
-		for child in deslist:
-			if child in term_direct_gene_map:
-				term_gene_set = term_gene_set | term_direct_gene_map[child]
+        deslist = nxadag.descendants(dG, term)
 
-		# jisoo
-		if len(term_gene_set) == 0:
-			print('There is empty terms, please delete term:', term)
-			sys.exit(1)
-		else:
-			term_size_map[term] = len(term_gene_set)
+        for child in deslist:
+            if child in term_direct_gene_map:
+                term_gene_set = term_gene_set | term_direct_gene_map[child]
 
-	leaves = [n for n in dG.nodes if dG.in_degree(n) == 0]
-	#leaves = [n for n,d in dG.in_degree() if d==0]
+        # jisoo
+        if len(term_gene_set) == 0:
+            print('There is empty terms, please delete term:', term)
+            sys.exit(1)
+        else:
+            term_size_map[term] = len(term_gene_set)
 
-	uG = dG.to_undirected()
-	connected_subG_list = list(nxacc.connected_components(uG))
+    leaves = [n for n in dG.nodes if dG.in_degree(n) == 0]
+    #leaves = [n for n,d in dG.in_degree() if d==0]
 
-	print('There are', len(leaves), 'roots:', leaves[0])
-	print('There are', len(dG.nodes()), 'terms')
-	print('There are', len(connected_subG_list), 'connected componenets')
+    uG = dG.to_undirected()
+    connected_subG_list = list(nxacc.connected_components(uG))
 
-	if len(leaves) > 1:
-		print('There are more than 1 root of ontology. Please use only one root.')
-		sys.exit(1)
-	if len(connected_subG_list) > 1:
-		print( 'There are more than connected components. Please connect them.')
-		sys.exit(1)
+    print('There are', len(leaves), 'roots:', leaves[0])
+    print('There are', len(dG.nodes()), 'terms')
+    print('There are', len(connected_subG_list), 'connected componenets')
 
-	return dG, leaves[0], term_size_map, term_direct_gene_map
+    if len(leaves) > 1:
+        print('There are more than 1 root of ontology. Please use only one root.')
+        sys.exit(1)
+    if len(connected_subG_list) > 1:
+        print('There are more than connected components. Please connect them.')
+        sys.exit(1)
+
+    return dG, leaves[0], term_size_map, term_direct_gene_map
 
 
 def load_train_data(file_name, cell2id, drug2id):
-	feature = []
-	label = []
+    feature = []
+    label = []
 
-	with open(file_name, 'r') as fi:
-		for line in fi:
-			tokens = line.strip().split('\t')
+    with open(file_name, 'r') as fi:
+        for line in fi:
+            tokens = line.strip().split('\t')
 
-			feature.append([cell2id[tokens[0]], drug2id[tokens[1]]])
-			label.append([float(tokens[2])])
+            feature.append([cell2id[tokens[0]], drug2id[tokens[1]]])
+            label.append([float(tokens[2])])
 
-	return feature, label
+    return feature, label
 
 
 def prepare_predict_data(test_file, cell2id_mapping_file, drug2id_mapping_file):
 
-	# load mapping files
-	cell2id_mapping = load_mapping(cell2id_mapping_file)
-	drug2id_mapping = load_mapping(drug2id_mapping_file)
+    # load mapping files
+    cell2id_mapping = load_mapping(cell2id_mapping_file)
+    drug2id_mapping = load_mapping(drug2id_mapping_file)
 
-	test_feature, test_label = load_train_data(test_file, cell2id_mapping, drug2id_mapping)
+    test_feature, test_label = load_train_data(
+        test_file, cell2id_mapping, drug2id_mapping)
 
-	print('Total number of cell lines = %d' % len(cell2id_mapping))
-	print('Total number of drugs = %d' % len(drug2id_mapping))
+    print('Total number of cell lines = %d' % len(cell2id_mapping))
+    print('Total number of drugs = %d' % len(drug2id_mapping))
 
-	return (torch.Tensor(test_feature), torch.Tensor(test_label)), cell2id_mapping, drug2id_mapping
+    return (torch.Tensor(test_feature), torch.Tensor(test_label)), cell2id_mapping, drug2id_mapping
 
 
 def load_mapping(mapping_file):
 
-	mapping = {}
+    mapping = {}
 
-	file_handle = open(mapping_file)
+    file_handle = open(mapping_file)
 
-	for line in file_handle:
-		line = line.rstrip().split()
-		mapping[line[1]] = int(line[0])
+    for line in file_handle:
+        line = line.rstrip().split()
+        mapping[line[1]] = int(line[0])
 
-	file_handle.close()
-	
-	return mapping
+    file_handle.close()
+
+    return mapping
 
 
 def prepare_train_data(train_file, test_file, cell2id_mapping_file, drug2id_mapping_file):
 
-	# load mapping files
-	cell2id_mapping = load_mapping(cell2id_mapping_file)
-	drug2id_mapping = load_mapping(drug2id_mapping_file)
+    # load mapping files
+    cell2id_mapping = load_mapping(cell2id_mapping_file)
+    drug2id_mapping = load_mapping(drug2id_mapping_file)
 
-	train_feature, train_label = load_train_data(train_file, cell2id_mapping, drug2id_mapping)
-	test_feature, test_label = load_train_data(test_file, cell2id_mapping, drug2id_mapping)
+    train_feature, train_label = load_train_data(
+        train_file, cell2id_mapping, drug2id_mapping)
+    test_feature, test_label = load_train_data(
+        test_file, cell2id_mapping, drug2id_mapping)
 
-	print('Total number of cell lines = %d' % len(cell2id_mapping))
-	print('Total number of drugs = %d' % len(drug2id_mapping))
+    print('Total number of cell lines = %d' % len(cell2id_mapping))
+    print('Total number of drugs = %d' % len(drug2id_mapping))
 
-	return (torch.Tensor(train_feature), torch.FloatTensor(train_label), torch.Tensor(test_feature), torch.FloatTensor(test_label)), cell2id_mapping, drug2id_mapping
+    return (torch.Tensor(train_feature), torch.FloatTensor(train_label), torch.Tensor(test_feature), torch.FloatTensor(test_label)), cell2id_mapping, drug2id_mapping
 
 
 def build_input_vector(input_data, cell_features, drug_features):
-	genedim = len(cell_features[0,:])
-	drugdim = len(drug_features[0,:])
-	feature = np.zeros((input_data.size()[0], (genedim+drugdim)))
+    genedim = len(cell_features[0, :])
+    drugdim = len(drug_features[0, :])
+    feature = np.zeros((input_data.size()[0], (genedim+drugdim)))
 
-	for i in range(input_data.size()[0]):
-		feature[i] = np.concatenate((cell_features[int(input_data[i,0])], drug_features[int(input_data[i,1])]), axis=None)
+    for i in range(input_data.size()[0]):
+        feature[i] = np.concatenate((cell_features[int(
+            input_data[i, 0])], drug_features[int(input_data[i, 1])]), axis=None)
 
-	feature = torch.from_numpy(feature).float()
-	return feature
-
+    feature = torch.from_numpy(feature).float()
+    return feature
