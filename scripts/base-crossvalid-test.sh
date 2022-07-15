32b5634174d9bf585c2d95215300a63767f4628b
kaixin <kaixinzhang@pku.edu.cn>
2022-07-15 03:28:34 -0700

    update baseline config

--- scripts/base-crossvalid-test.sh
+++ scripts/base-crossvalid-test.sh
@@ -1,33 +1,34 @@
-ckpt0='../../../ckpt/cv0/ckpt-epoch299.pt'
-ckpt1='../../../ckpt/cv1/ckpt-epoch263.pt'
-ckpt2='../../../ckpt/cv2/ckpt-epoch297.pt'
-ckpt3='../../../ckpt/cv3/ckpt-epoch80.pt'
-ckpt4='../../../ckpt/cv4/ckpt-epoch80.pt'
+ckpt0='../../../ckpt/baseline/cv0/ckpt-epoch50.pt'
+ckpt1='../../../ckpt/baseline/cv1/ckpt-epoch50.pt'
+ckpt2='../../../ckpt/baseline/cv2/ckpt-epoch185.pt'
+ckpt3='../../../ckpt/baseline/cv3/ckpt-epoch80.pt'
+ckpt4='../../../ckpt/baseline/cv4/ckpt-epoch80.pt'
 data0='../../../data/cross_valid/drugcell_split_1.txt'
 data1='../../../data/cross_valid/drugcell_split_2.txt'
 data2='../../../data/cross_valid/drugcell_split_3.txt'
 data3='../../../data/cross_valid/drugcell_split_4.txt'
 data4='../../../data/cross_valid/drugcell_split_5.txt'
-output0='hydra.run.dir=./outputs/cross_valid_test/0/'
-output1='hydra.run.dir=./outputs/cross_valid_test/1/'
-output2='hydra.run.dir=./outputs/cross_valid_test/2/'
-output3='hydra.run.dir=./outputs/cross_valid_test/3/'
-output4='hydra.run.dir=./outputs/cross_valid_test/4/'
+output0='hydra.run.dir=./outputs/cross_valid_test/base_0/'
+output1='hydra.run.dir=./outputs/cross_valid_test/base_1/'
+output2='hydra.run.dir=./outputs/cross_valid_test/base_2/'
+output3='hydra.run.dir=./outputs/cross_valid_test/base_3/'
+output4='hydra.run.dir=./outputs/cross_valid_test/base_4/'
 output5='hydra.run.dir=./outputs/cross_valid_test/-1/'
 
 tflag='--cfg job'
 c0='trainer.device=cuda:0'
 c1='trainer.device=cuda:1'
 pearson='trainer.epoch_criterion=pearson'
-"""
+
 python src/test.py data.test.path=$data0 trainer.ckpt=$ckpt0 $output0
 
 python src/test.py data.test.path=$data1 trainer.ckpt=$ckpt1 $output1
 
 python src/test.py data.test.path=$data2 trainer.ckpt=$ckpt2 $output2
-
+test="""
 python src/test.py data.test.path=$data3 trainer.ckpt=$ckpt3 $output3
 
 python src/test.py data.test.path=$data4 trainer.ckpt=$ckpt4 $output4
-"""
+
 python src/test.py data.test.path=../../../data/drugcell_all.txt trainer.ckpt=../../../ckpt/dc_v1.pt $output5
+"""
--- scripts/base-crossvalid-train.sh
+++ scripts/base-crossvalid-train.sh
@@ -13,15 +13,17 @@ save1='trainer.save_dir=../../../ckpt/baseline/cv1'
 save2='trainer.save_dir=../../../ckpt/baseline/cv2'
 save3='trainer.save_dir=../../../ckpt/baseline/cv3'
 save4='trainer.save_dir=../../../ckpt/baseline/cv4'
+ckpt0='trainer.ckpt=../../../ckpt/baseline/cv0/ckpt-epoch35.pt'
+ckpt1='trainer.ckpt=../../../ckpt/baseline/cv1/ckpt-epoch35.pt'
 c0='trainer.device=cuda:0'
 c1='trainer.device=cuda:1'
 notest='data.test=null'
 
-nohup python src/train.py +kay=cross ~data.train.path.0 ~data.val.path.0 $notest $save0 $c1 $output0 > ./outputs/base_cross_validation/0.txt &
+#nohup python src/train.py +kay=cross ~data.train.path.0 ~data.val.path.0 $notest $save0 $c1 $ckpt0 $output0 > ./outputs/base_cross_validation/0.txt &
 
-nohup python src/train.py +kay=cross ~data.train.path.1 ~data.val.path.1 $notest $save1 $c1 $output1 > ./outputs/base_cross_validation/1.txt &
+#nohup python src/train.py +kay=cross ~data.train.path.1 ~data.val.path.1 $notest $save1 $c1 $ckpt1 $output1 > ./outputs/base_cross_validation/1.txt &
 
-#nohup python src/train.py +kay=cross ~data.train.path.2 ~data.val.path.2 $notest $save2 $c1 $output2 > ./outputs/base_cross_validation/2.txt &
+nohup python src/train.py +kay=cross ~data.train.path.2 ~data.val.path.2 $notest $save2 $c0 $output2 > ./outputs/base_cross_validation/2.txt &
 
 #nohup python src/train.py +kay=cross ~data.train.path.3 ~data.val.path.3 $notest $save3 $c0 $output3 > ./outputs/base_cross_validation/3.txt &
 
